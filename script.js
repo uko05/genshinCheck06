eb9b00cf4eeefd8b3d207bfb37c7e7fb0c3adc6f
uko05 <yosieyosie1@yahoo.co.jp>
2025-12-31 14:53:05 +0900

    Move index.html to the root directory for GitHub Pages

--- script.js
+++ script.js
@@ -116,6 +116,7 @@ const imageData = [
     { src: 'hotaru.png', category: 'chara' },
     { src: 'sora.png', category: 'chara' },
     
+    { src: 'version/luna4.png', category: 'version' },
     { src: 'version/luna3.png', category: 'version' },
     { src: 'version/luna2.png', category: 'version' },
     { src: 'version/luna1.png', category: 'version' },
